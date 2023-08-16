4eaf23bcf820739478b3a7694407fe7c39253619
Erin Geier <erin.geier@gmail.com>
2023-08-15 22:50:59 -0500

    test query committed to test branch in DBeaver

---
+++ Scripts/Test2.sql
@@ -0,0 +1,20 @@
+--Total purchase sales income per dealership
+
+
+SELECT DISTINCT
+	s.dealership_id,
+	d.business_name,
+	SUM(s.price) OVER(PARTITION BY s.dealership_id) AS total_sales_income
+FROM sales s
+NATURAL JOIN dealerships d
+ORDER BY s.dealership_id;
+	
+	
+SELECT 
+    s.dealership_id,
+    d.business_name,
+    SUM(s.price) AS total_sales_income 
+FROM sales s
+NATURAL JOIN dealerships d
+GROUP BY s.dealership_id, d.business_name
+ORDER BY s.dealership_id ;
